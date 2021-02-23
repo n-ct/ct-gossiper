321b42beea0546b31d3802becee737f78b4f348e
Federico Cedolini <fcedolin@trincoll.edu>
2021-02-23 14:51:20 -0500

    CTObject, test update. Disable PoM gossip
    Structs and functions are now called from the monitor.
    The test now send a CTObject.
    PoM gossips is disable for now.

--- gossiper/gossiper.go
+++ gossiper/gossiper.go
@@ -11,18 +11,23 @@ import (
 	"io/ioutil"
 	"strings"
 	"flag"
+
 	"github.com/golang/glog"
 	cto "ct-gossiper"
+	mtr "github.com/n-ct/ct-monitor"
+	mtrList "github.com/n-ct/ct-monitor/entitylist"
 	mtrUtils "github.com/n-ct/ct-monitor/utils"
-	mtr "github.com/n-ct/ct-monitor/entitylist"
 )
 
-var peers []*mtr.Monitor;
+const(
+	GossipPath = "/ct/v1/gossip"
+)
 
+var peers []*mtrList.MonitorInfo;
 var messages cto.MessagesMap; //[TypeID][subjectOrSigner][Timestamp][Version]
 var alertsMap cto.MessagesMap;//[Subject][Signer][Timestamp][Version]
 var port string;
-var allMonitors *mtr.MonitorsList;
+var allMonitors *mtrList.MonitorList;
 
 
 func main() {
@@ -55,7 +60,7 @@ func main() {
 
 	gossiperSetup(*configFilename, *monitorsFilename);
 
-	http.HandleFunc("/ct/v1/gossip", GossipHandler); // call GossipHandler on post to /gossip
+	http.HandleFunc(GossipPath, GossipHandler); // call GossipHandler on post to /gossip
 
 	glog.Infof("Starting server on %v\n", port);
 
@@ -68,7 +73,7 @@ func main() {
 // GossipHandler is called on a post request to /ct/v1/gossip.
 // It handles the logic of gossip within a network system
 func GossipHandler(w http.ResponseWriter, req *http.Request){
-	data := cto.CTObject{};
+	data := mtr.CTObject{};
 	err := json.NewDecoder(req.Body).Decode(&data); // fill that struct using the JSON encoded struct send via the post
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest) // if there is an eror report and abort
@@ -84,20 +89,20 @@ func GossipHandler(w http.ResponseWriter, req *http.Request){
 		workingMap = messages;
 	}
 
-	glog.Infof("CTObject recived from source: %s\n\n", data.ToDebugString()); //print contents of message (for debugging)
+	glog.Infof("CTObject recived from source: %s\n\n", cto.ToDebugString(data)); //print contents of message (for debugging)
 	if message, ok := workingMap[identifier.First][identifier.Second][identifier.Third][identifier.Fourth]; ok { // if I have the message already check for conflict
-		if bytes.Compare(data.Hash, message.Hash)==0 {
+		if bytes.Compare(data.Digest, message.Digest)==0 {
 			http.Error(w, "Duplicate item\n", http.StatusBadRequest); // if no conflic send back "duplicate item", and bad request status code to sender
 		} else {
 			glog.Infof("Misbehavior detected\n\n"); // if conflict send a PoM to all peers
-			PoM := cto.NewCTObject("PoM", 0, []byte{0,1,2,3}, ""); //new dummy Proof of misbehavior
-			addEntry(messages, *PoM, PoM.Identifier()); // store PoM
-
-			//gossip new PoM to all peers
-			for _, peer := range peers{
-				glog.Infof("Gossiping PoM to peer: %v\n", peer.Id);
-				post(fmt.Sprintf("%v/ct/v1/gossip", peer.Gossip), PoM, false);
-			}
+			// PoM := cto.NewCTObject("PoM", 0, []byte{0,1,2,3}, ""); //new dummy Proof of misbehavior
+			// addEntry(messages, *PoM, PoM.Identifier()); // store PoM
+			//
+			// //gossip new PoM to all peers
+			// for _, peer := range peers{
+			// 	glog.Infof("Gossiping PoM to peer: %v\n", peer.Id);
+			// 	post(fmt.Sprintf("%v/ct/v1/gossip", peer.Gossip), PoM, false);
+			// }
 		}
 	}else{
 		if data.Blob == nil{ //If the message does not contain the blob
@@ -107,8 +112,8 @@ func GossipHandler(w http.ResponseWriter, req *http.Request){
 			addEntry(workingMap, data, identifier);// if message is new add it to messages map
 
 			for _, peer := range peers{
-				glog.Infof("Gossiping new info to peer: %v\n", peer.Id);
-				post(fmt.Sprintf("%v/ct/v1/gossip", peer.Gossip), &data, false);
+				glog.Infof("Gossiping new info to peer: %v\n", peer.MonitorID);
+				post(mtrUtils.CreateRequestURL(peer.GossiperURL, GossipPath), &data, false);
 			}
 		}
 	}
@@ -116,12 +121,12 @@ func GossipHandler(w http.ResponseWriter, req *http.Request){
 
 // post takes in an address as a string and a pointer to a CTObject struct
 // and makes a post request to that address with the JSON encoded version of that struct
-func post(address string, data *cto.CTObject, withBlob bool){
-	var toSend *cto.CTObject;
+func post(address string, data *mtr.CTObject, withBlob bool){
+	var toSend *mtr.CTObject;
 	if withBlob {
 		toSend = data;
 	} else {
-		toSend = data.CopyWithoutBlob();
+		toSend = cto.CopyWithoutBlob(data);
 	}
 	var jsonStr, _ = json.Marshal(toSend);
 
@@ -151,15 +156,15 @@ func post(address string, data *cto.CTObject, withBlob bool){
 }
 
 //addEntry adds a new entry to the selected map using the data identifier as keys
-func addEntry(dataMap cto.MessagesMap, data cto.CTObject, identifier cto.ObjectIdentifier){
+func addEntry(dataMap cto.MessagesMap, data mtr.CTObject, identifier mtr.ObjectIdentifier){
 	if _, ok := dataMap[identifier.First]; !ok {
-		dataMap[identifier.First] = make(map[string]map[uint64]map[string] *cto.CTObject);
+		dataMap[identifier.First] = make(map[string]map[uint64]map[string] *mtr.CTObject);
 	}
 	if _, ok := dataMap[identifier.First][identifier.Second]; !ok {
-		dataMap[identifier.First][identifier.Second] = make(map[uint64]map[string] *cto.CTObject);
+		dataMap[identifier.First][identifier.Second] = make(map[uint64]map[string] *mtr.CTObject);
 	}
 	if _, ok := dataMap[identifier.First][identifier.Second][identifier.Third]; !ok {
-		dataMap[identifier.First][identifier.Second][identifier.Third] = make(map[string] *cto.CTObject);
+		dataMap[identifier.First][identifier.Second][identifier.Third] = make(map[string] *mtr.CTObject);
 	}
 	dataMap[identifier.First][identifier.Second][identifier.Third][identifier.Fourth] = &data;
 }
@@ -169,15 +174,15 @@ func gossiperSetup(configFilename string, monitorsFilename string){
 	gossipConfig := cto.NewGossipConfig(configFilename);
 
 	getPeers(gossipConfig, monitorsFilename);
-	port = strings.Split(allMonitors.FindMonitorByID(gossipConfig.Monitor_id).Gossip, ":")[2];
+	port = strings.Split(allMonitors.FindMonitorByMonitorID(gossipConfig.Monitor_id).GossiperURL, ":")[2];
 
 }
 
 //getPeers get all monitors from file and populates the peers with the respective monitors
-func getPeers(gossipConfig cto.GossipConfig, monitorsFilename string)  {
-	allMonitors = mtr.NewMonitorList(monitorsFilename)
+func getPeers(gossipConfig *cto.GossipConfig, monitorsFilename string)  {
+	allMonitors = mtrList.NewMonitorList(monitorsFilename)
 
 	for _, monitorId := range gossipConfig.Monitors_ids {
-		peers = append(peers, allMonitors.FindMonitorByID(monitorId));
+		peers = append(peers, allMonitors.FindMonitorByMonitorID(monitorId));
 	}
 }
--- types.go
+++ types.go
@@ -1,81 +1,31 @@
 package CTObject
 
 import (
-	"crypto/sha256"
 	"fmt"
-	"strings"
 	"encoding/json"
 	mtrUtils "github.com/n-ct/ct-monitor/utils"
+	mtr "github.com/n-ct/ct-monitor"
 )
 
-// function to compute sha256 of a []byte
-func NewSHA256(data []byte) []byte {
-	hash := sha256.Sum256(data)
-	return hash[:]
-}
+type MessagesMap map[string]map[string]map[uint64]map[string] *mtr.CTObject;
 
-type MessagesMap map[string]map[string]map[uint64]map[string] *CTObject;
-
-type CTObject struct{
-  TypeID string
-  Version string
-  Timestamp uint64
-  Signer string
-  Hash []byte
-  Subject string
-  Blob []byte
-}
-
-func NewCTObject(TypeID string, Timestamp uint64, Blob []byte, Subject string) (*CTObject){
-	cto := CTObject{};
-	cto.TypeID = TypeID;
-  cto.Timestamp = Timestamp;
-	cto.Signer = "";
-  cto.Hash = NewSHA256(Blob);
-	cto.Blob = Blob;
-	cto.Subject = Subject;
-	return &cto;
-}
 
 // returns a pointer to a new CTObject struct with all the same elements but with the 'Blob' field set to nil.
-func (data *CTObject) CopyWithoutBlob() (*CTObject){
-	cto := CTObject{};
+func CopyWithoutBlob(data *mtr.CTObject) (*mtr.CTObject){
+	cto := mtr.CTObject{};
 	cto.TypeID = data.TypeID;
+	cto.Version = data.Version;
 	cto.Timestamp = data.Timestamp;
 	cto.Signer = data.Signer;
-	cto.Hash = data.Hash;
-	cto.Blob = nil; //blob is nil
 	cto.Subject = data.Subject;
+	cto.Digest = data.Digest;
+	cto.Blob = nil; //blob is nil
 	return &cto;
 }
 
 //return the information from CTObject for printing
-func (data *CTObject) ToDebugString() (string){
-	return fmt.Sprintf("CTObject{\n\tTypeID:%v\n\tTimestamp:%v\n\tHash:%v\n\tBlob:%v\n}", data.TypeID, data.Timestamp, data.Hash, data.Blob);
-}
-
-type ObjectIdentifier struct{
-	First string
-	Second string
-	Third uint64
-	Fourth string
-}
-
-//creates identifier for each type of CTObject
-//Alerts [Subject][Signer][Timestamp][Version]
-//The rest [TypeID][Subject|Signer][Timestamp][Version]
-func (data *CTObject) Identifier() (ObjectIdentifier){
-	if data.TypeID == "Alert"{
-		return ObjectIdentifier{First: data.Subject, Second: data.Signer, Third: data.Timestamp, Fourth: data.Version,};
-	}
-
-	var subjectOrSigner string;
-	if len(data.Subject) == 0 {
-		subjectOrSigner = data.Signer;
-	} else {
-		subjectOrSigner = data.Subject;
-	}
-	return ObjectIdentifier{First: data.TypeID, Second: subjectOrSigner, Third: data.Timestamp, Fourth: data.Version,};
+func ToDebugString(data mtr.CTObject) (string){
+	return fmt.Sprintf("CTObject{\n\tTypeID:%v\n\tTimestamp:%v\n\tDigest:%v\n\tBlob:%v\n}", data.TypeID, data.Timestamp, data.Digest, data.Blob);
 }
 
 //structs for JSON files
@@ -86,58 +36,10 @@ type GossipConfig struct{
 
 func NewGossipConfig (filename string) (*GossipConfig) {
 	var gossipConfig GossipConfig;
-	byteData := mtrUtils.JSONFiletoBytes(configFilename);
+	byteData := mtrUtils.JSONFiletoBytes(filename);
 	if err := json.Unmarshal(byteData, &gossipConfig); err != nil {
 		fmt.Errorf("Failed to parse gossip configuration: %v", err);
 		return nil;
 	}
-	return gossipConfig;
+	return &gossipConfig;
 }
-/*
-type MonitorsList struct {
-	Operators []*Operator `json:"operators"`
-}
-
-type Operator struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Monitors []*Monitor `json:"monitors"`
-}
-
-type Monitor struct {
-	Id string	`json:"monitor_id"`
-	Key string 	`json:"monitor_key"`
-	Url string	`json:"monitor_url"`
-	Gossip string	`json:"gossiper_url"`
-}
-
-func NewMonitorList(monitorsFilename string) *MonitorsList{
-	byteData := mtrUtils.JSONFiletoBytes(monitorsFilename)
-	monitorList, err := NewFromJSON(byteData)
-	if err != nil {
-		return nil
-	}
-	return monitorList
-}
-
-// NewFromJSON creates a MonitorList from JSON data.
-func NewFromJSON(data []byte) (*MonitorsList, error) {
-	var ml MonitorsList
-	if err := json.Unmarshal(data, &ml); err != nil {
-		return nil, fmt.Errorf("Failed to parse monitor list: %v", err)
-	}
-	return &ml, nil
-}
-
-//Find and return specific monitor based on its ID
-func (ml *MonitorsList) FindMonitorByID(monitorID string) (*Monitor) {
-	for _, op := range ml.Operators {
-		for _, monitor := range op.Monitors {
-			if (strings.Contains(monitor.Id, monitorID)){
-				return monitor
-			}
-		}
-	}
-	return nil
-}
-*/
